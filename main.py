4fa8b34253f16f6944bec9fb7e25aedc162b6f53
Ad450 <adjeimanue@gmail.com>
2024-07-13 22:21:31 +0000

    minor additions

--- main.py
+++ main.py
@@ -2,39 +2,33 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
-class UserDto(BaseModel):
-    username: str
-    age: int | None
-    email: str
-    profile_picture: str | None
-
 
 app = FastAPI()
 
-users: list[UserDto] = []
 
 
-@app.get(path="/hello")
-def greet_hello():
-    return {"Greeting": "hello"}
+
+# @app.get(path="/hello")
+# def greet_hello():
+#     return {"Greeting": "hello"}
 
 
-@app.get(path="/hello/{name}")
-def greet_you(name: str, age: int | None = None):
-    return {
-        "Greeting": f"Hellow {name}",
-        "Age": age if age is not None else "No age given"
-    }
+# @app.get(path="/hello/{name}")
+# def greet_you(name: str, age: int | None = None):
+#     return {
+#         "Greeting": f"Hellow {name}",
+#         "Age": age if age is not None else "No age given"
+#     }
 
 
-@app.get(path="/process")
-def greet_you(name: str | None = None):
-    if name:
-        return {"Process": f"Processing {name}"}
-    return {"Process": "oops no name"}
+# @app.get(path="/process")
+# def greet_you(name: str | None = None):
+#     if name:
+#         return {"Process": f"Processing {name}"}
+#     return {"Process": "oops no name"}
 
 
-@app.post(path="/user")
-def create_user(user: UserDto):
-    users.append(user)
-    return {"result": "user created"}
+# @app.post(path="/user")
+# def create_user(user: UserDto):
+#     users.append(user)
+#     return {"result": "user created"}
